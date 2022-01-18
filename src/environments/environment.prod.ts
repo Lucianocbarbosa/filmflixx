f12ef918030207824afea99c997b449e795859e7
Lucianocbarbosa <lucianobebarbosa@gmail.com>
2022-01-18 18:18:07 -0300

    Projeto Angular Plataforma de Filmes BWC9 SoulCode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'filmflixx-dd644',
+    appId: '1:697314710196:web:7812aaf07fbf992dcebde1',
+    storageBucket: 'filmflixx-dd644.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCIwMJC4KH2a7XLTMHyrZT3qQSXd0p-QLg',
+    authDomain: 'filmflixx-dd644.firebaseapp.com',
+    messagingSenderId: '697314710196',
+  },
   production: true
 };
